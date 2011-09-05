ad726ad5464179d1a1fdb4cbc478fb43a5931739
sharathjagannath <sharathjagannath@gmail.com>
2011-09-05 12:46:02 +0000

    T-Box bug fix and created config file

--- freebaseRDFizer/src/main/java/com/sj/freebase/data/rdf/DemoFreebaseRdfizer.java
+++ freebaseRDFizer/src/main/java/com/sj/freebase/data/rdf/DemoFreebaseRdfizer.java
@@ -18,10 +18,13 @@ import org.apache.log4j.Logger;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 
+import com.hp.hpl.jena.ontology.OntModel;
 import com.sj.data.transform.MalFormedAssertionException;
 import com.sj.data.transform.SkippedAssertionException;
 import com.sj.freebase.data.rdf.FreebaseRdfizer;
 import com.sj.freebase.schema.rdf.FbSchemaGlobals;
+import com.sj.freebase.utils.ConfigUtils;
+import com.sj.ontology.utils.FreebaseOntologyCreationUtils;
 
 public class DemoFreebaseRdfizer {
 
@@ -36,21 +39,22 @@ public class DemoFreebaseRdfizer {
 
     public static void main(String [] args) throws Exception {
         logger.setLevel(Level.INFO);
-        List<String> predicatesToSkip = new ArrayList<String>();
-        predicatesToSkip.add("common.topic.notable_for");
+        Set<String> predicatesToSkip = ConfigUtils.getPropertiesToSkip();
+        predicatesToSkip.addAll(ConfigUtils.getTypesToSkip());
+        Set<String> domainsToSkip = ConfigUtils.getDomainsToSkip();
 
-        Set<String> domainsToSkip = new HashSet<String>();
-        domainsToSkip.add("music");
+        OntModel model =
+            FreebaseOntologyCreationUtils.getOntologyModel(domainsToSkip);
         FreebaseRdfizer rdfizer =
             new FreebaseRdfizer(null, null, predicatesToSkip, null,
-                domainsToSkip, new FileInputStream(new File(
-                    FbSchemaGlobals.FREEBASE_ONTOLOGY_PATH)));
+                domainsToSkip, model);
 
         if (args.length < 1 || args.length % 2 != 0) {
             throw new IllegalArgumentException(
                 "Usage: DemoFreebaseRdfizer list{<tsv file> <converted rdf file name>}");
         }
 
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < args.length; i++) {
 
             BufferedReader reader =
@@ -68,14 +72,17 @@ public class DemoFreebaseRdfizer {
                 try {
                     // display (rdfizer.transformData(assertion));
 
-                    List<StringBuffer> triples =
-                        rdfizer.transformData(assertion);
-                    for (StringBuffer triple : triples) {
-                        writer.write(triple.toString() + "\n");
+                    List<String> triples = rdfizer.transformData(assertion);
+                    for (String triple : triples) {
+                        System.out.println(triple);
+                        // writer.write(triple.toString() + "\n");
                     }
                 } catch (NullPointerException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
+                    model.close();
+                    reader.close();
+                    writer.close();
                     throw e;
                 } catch (MalFormedAssertionException e) {
                     // TODO Auto-generated catch block
@@ -85,12 +92,20 @@ public class DemoFreebaseRdfizer {
                 } catch (SkippedAssertionException e) {
                     // TODO Auto-generated catch block
                     // System.out.println (e.getAssertion());
-                    logger.warn("Skipped Assertion:" + e.getAssertion());
-                }
+                    if (e.toString().contains("/m/03frbs5")) {
+                        throw e;
+                    }
+                    logger.warn("Skipped Assertion:   ---   " +
+                        e.getAssertion());
+                } 
             }
             reader.close();
             writer.close();
         }
+        
+        model.close();
+        System.out.println("Total time taken : " +
+            (System.currentTimeMillis() - startTime));
 
     }
 }
