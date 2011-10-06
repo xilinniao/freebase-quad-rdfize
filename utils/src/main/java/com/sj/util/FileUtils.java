fc669548a5ca059df1acdecd66136284ac4c41c7
sharathjagannath <sharathjagannath@gmail.com>
2011-10-06 06:32:22 +0000

    committing utils

---
+++ utils/src/main/java/com/sj/util/FileUtils.java
@@ -0,0 +1,38 @@
+package com.sj.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.Logger;
+
+public class FileUtils {
+    private static Logger logger = Logger.getLogger(FileUtils.class);
+
+    // TODO : Handle comments
+    public static String readRdfizerConfig(String fileName) {
+
+        String json = "";
+
+        try {
+            FileInputStream in = new FileInputStream(new File(fileName));
+            BufferedReader br =
+                new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = br.readLine()) != null) {
+                json = StringUtils.combine(json, line);
+            }
+        } catch (Exception e) {
+            logger.debug("Failed to read file : " + fileName);
+        }
+
+        if (json == null) {
+            json = "";
+        }
+
+        return json;
+    }
+}
