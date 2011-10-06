ce734ea6e68bebc33dd9480b5df9e9a0dcf36b31
sharathjagannath@gmail.com <sharathjagannath@gmail.com@346b42cc-9820-ba8e-244e-e89b93a0bb61>
2011-10-06 08:40:16 +0000

    no more schema fetch during run time

---
+++ trunk/freebaseRDFizer/src/main/java/com/sj/freebase/data/constants/RdfizerConstants.java
@@ -0,0 +1,8 @@
+package com.sj.freebase.data.constants;
+
+public class RdfizerConstants {
+    public static final String FREEBASE_NAMESPACE =
+        "http://rdf.freebase.com/ns/";
+    public static final String SCHEMA_FILE_PATH="/freebase/config/schema.rdfs";
+    public static final String RELATIVE_SCHEMA_FILE_PATH="src/main/resources/schema.rdfs";
+}
